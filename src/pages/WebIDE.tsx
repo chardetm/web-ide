7f193d9b579cfe890456019184cf04c96d29e4ec
Maverick Chardet <maverick.chardet@ac-paris.fr>
2025-02-04 16:55:51 +0100

    Revert "Removed Roboto font from build"
    This reverts commit d83f9668c04d91e96e761eb6eeb6812c7d8f6585.

--- src/pages/WebIDE.tsx
+++ src/pages/WebIDE.tsx
@@ -1,5 +1,10 @@
 import { useState, useEffect, useCallback } from "react";
 
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 import styles from "./WebIDE.module.scss";
 
 import CodeEditorWindow from "../windows/code-editor";
