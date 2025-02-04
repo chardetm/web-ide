d83f9668c04d91e96e761eb6eeb6812c7d8f6585
Maverick Chardet <maverick.chardet@ac-paris.fr>
2025-02-04 14:49:04 +0100

    Removed Roboto font from build

--- src/pages/WebIDE.tsx
+++ src/pages/WebIDE.tsx
@@ -1,10 +1,5 @@
 import { useState, useEffect, useCallback } from "react";
 
-import "@fontsource/roboto/300.css";
-import "@fontsource/roboto/400.css";
-import "@fontsource/roboto/500.css";
-import "@fontsource/roboto/700.css";
-
 import "./WebIDE.scss";
 import styles from "./WebIDE.module.scss";
 
